b642aedcd4db2d84a5b0223d2ba3f9fe4671a613
Dmitrii Sobin <neo7976@yandex.ru>
2022-06-06 15:27:27 +0300

    Revert "Тестируем мои изменения"
    This reverts commit 66fa800ccf6a85f49489ba313151692ad741f7fa.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         System.out.println("Hello world");
         Person person = new Person();
         Person person2 = new Person();
-        Person person3 = new Person();
 
 
     }
