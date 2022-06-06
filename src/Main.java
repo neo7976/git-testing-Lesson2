234e5ea0a10f9aa4b3395d940f178f89fc2c56fd
Dmitrii Sobin <neo7976@yandex.ru>
2022-06-06 15:27:09 +0300

    Тестируем мои изменения

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello world");
         Person person = new Person();
         Person person2 = new Person();
         Person person3 = new Person();
