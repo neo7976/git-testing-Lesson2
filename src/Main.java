efc6d658edf307ff15887c4ee46fd434fa8859f6
Dmitrii Sobin <neo7976@yandex.ru>
2022-06-06 13:28:17 +0300

    Revert "New"
    This reverts commit 02bc8c0bc3e6361daef247deb7fbf7eb3ce68db7.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello world");
-        Person person = new Person();
-        Person person2 = new Person();
-
 
     }
 }
